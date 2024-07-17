fa52dc6b865849f848a612fee8516440878983d5
Slavisa Markovic <sslavishaa@gmail.com>
2024-07-17 23:00:14 +0200

    tst

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"focus_helper.js": "f51d87d818a0f0acb49b05e77b42d977",
 "version.json": "ec16a443585e9f667f409f64605d0c6d",
-"index.html": "2ba973cd326b88f9c6f79aaf6e045c84",
-"/": "2ba973cd326b88f9c6f79aaf6e045c84",
-"main.dart.js": "26a97ed22f1666cf579c1d83c9f12328",
+"index.html": "af8b844dfcfd575533cb94f7dbb18cbb",
+"/": "af8b844dfcfd575533cb94f7dbb18cbb",
+"main.dart.js": "2f10ea2b965ec39b387fb4233f7c9d90",
 ".well-known/apple-developer-merchantid-domain-association": "29afa7e1d291721bc64c6c9fe93223b3",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "68cb4ed434ba1a72e39516427fcfff3e",
