80e93c9d703ba4e4d1c67b004a149e3c13d4f97a
Slavisa Markovic <sslavishaa@gmail.com>
2024-10-24 15:29:05 +0200

    change version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"focus_helper.js": "ac13e66eef3b33703e32558620d2dc60",
 "version.json": "ec16a443585e9f667f409f64605d0c6d",
-"index.html": "8465983a4c5261f1d26fcbe887215ca7",
-"/": "8465983a4c5261f1d26fcbe887215ca7",
-"main.dart.js": "a40c3bf88d5b5dd141ee645f6dbdd032",
+"index.html": "c3a28907bdf5d33319bd36deb20c2fa0",
+"/": "c3a28907bdf5d33319bd36deb20c2fa0",
+"main.dart.js": "e90903342e97b335c7c58f718cab5710",
 ".well-known/apple-developer-merchantid-domain-association": "29afa7e1d291721bc64c6c9fe93223b3",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "68cb4ed434ba1a72e39516427fcfff3e",
